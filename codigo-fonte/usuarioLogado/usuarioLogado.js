3b13675b35ff8d83a1b8908666ee6ca230b8430b
Milton Ventura <milton.venturapsi@gmail.com>
2024-06-02 19:23:44 -0300

    Ajustes na página usuarioLogado

--- codigo-fonte/usuarioLogado/usuarioLogado.js
+++ codigo-fonte/usuarioLogado/usuarioLogado.js
@@ -1,6 +1,33 @@
 console.log("Loa")
 
+let listaPrestadores = [];
 
+function addUsuario(nomeusr, distancia, prestServ, servsPrestados) {
+    listaUsuarios = retornaListaUsrs();
+    let usuario = {
+        nome: nomeusr,
+        distancia: distancia,
+        prestadorServico: prestServ,
+        servicosPrestados: servsPrestados || []
+    }
+    listaUsuarios.push(usuario);
+    localStorage.arrUsuarios = JSON.stringify(listaUsuarios);
+}
+
+function templateUsers() {
+    addUsuario("João Silveira", "12", true, [{ nomeServico: "Marido de Aluguel", avaliacao: 3, descricaoServico: "Pequenos serviços domésticos em geral, troca de chuveiro, torneira, tomada, lâmpadas, etc.", valor: 0 }]);
+
+    addUsuario("Maria Antonia", "3", true, [{ nomeServico: "Diarista", avaliacao: 4, descricaoServico: "Faço faxina em casas de família", valor: 50 }, { nomeServico: "Cozinheira", avaliacao: 5, descricaoServico: "Cozinho em eventos e festas. Comida caseira deliciosa!", valor: 60 }]);
+
+    addUsuario("José da Silva", "5", true, [{ nomeServico: "Encanador", avaliacao: 4, descricaoServico: "Presto serviço de encanador para pequenos reparos, conserto de vazamentos, troca de torneira, chuveiros, etc. Não realizo serviços onde será necessário quebrar paredes.", valor: 200.99 }]);
+
+    addUsuario("Denise Moreira", "3", false, []);
+}
+
+function limpaLocalStorage() {
+    listaUsuarios = []
+    localStorage.arrUsuarios = JSON.stringify(listaUsuarios);
+}
 
 function retornaListaUsrs() {
     let listaUsuarios = [];
@@ -10,47 +37,47 @@ function retornaListaUsrs() {
     return listaUsuarios;
 }
 
-function addUsuario(nomeusr, localUsr, prestServ, servsPrestados) {
-
-    listaUsuarios = retornaListaUsrs();
-
-    let usuario = {
-        nome: nomeusr,
-        local: localUsr,
-        prestadorServico: prestServ,
-        servicosPrestados: servsPrestados || []
-    }
-    listaUsuarios.push(usuario);
-    localStorage.arrUsuarios = JSON.stringify(listaUsuarios);
-}
-
 function retornaPrestadores() {
-
-    let listaPrestadores = [];
+    listaPrestadores = [];
     listaUsuarios = retornaListaUsrs();
 
-    for (var indice in listaUsuarios) {
-
-        if (listaUsuarios[indice].prestadorServico) {
-            listaPrestadores.push(listaUsuarios[indice]);
+    listaUsuarios.map((usuario) => {
+        if (usuario.prestadorServico) {
+            listaPrestadores.push(usuario)
         }
-    }
-
+    });
     return listaPrestadores;
 }
 
 function imprimePrestadores() {
     let conteudo = "";
-    conteudo += `<div class="usrcontainer">
-    <img src="imgs/userround.png" alt="Foto usuário redondo">
-    <p class="usrdscr">José da Silva, <strong>Encanador</strong> <img src="imgs/map-pin.png" alt="Map pin"> 5Km
-        <br><br>Presto serviço de encanador para pequenos reparos, conserto de vazamentos, troca de torneira,
-        chuveiros, etc.
-        Não realizo serviços onde será necessário quebrar paredes.
-    </p>
-    <p class="price">R$200,00 / hora</p>
-    <p class="pricenote">(preço médio)</p>
-    <img class="fourstars" src="imgs/4stars.png" alt="4 Estreas">
-    </div>`
-    usrcontainer.innerHTML = conteudo;
+    retornaPrestadores();
+    listaPrestadores.map((prestador) => {
+        prestador.servicosPrestados.map((servico) => {
+            let valor = "Valor a combinar";
+            if (servico.valor) {
+                valor = `R$${servico.valor.toFixed(2).replace(".", ",")} / hora`;
+            }
+            conteudo += `
+            <div class="prestadores">
+                <img src="imgs/userround.png" alt="Foto usuário redondo">
+                <div class="prestadoresdscr">
+                    <p>${prestador.nome}, <strong>${servico.nomeServico}</strong> <img src="imgs/map-pin.png" alt="Map pin">
+                        ${prestador.distancia}Km
+                        <br><br>${servico.descricaoServico}
+                    </p>
+                    <div class="cardpreco">
+                        <div class="stars"><img src="imgs/4stars.png" alt="Estralas de avaliação"></div>
+                        <p class="price">${valor}</p>
+                        <p class="pricenote">(preço médio)</p>
+                    </div>
+                </div>
+            </div>`
+        });
+    });
+    cardPrestadores.innerHTML = conteudo;
 }
+
+limpaLocalStorage();
+templateUsers();
+imprimePrestadores();
