e0095b306aa66af71658299e94aa2603562c8c8a
falima2004 <fabiano.lima.1527242@sga.pucminas.br>
2024-06-06 16:24:20 -0300

    Add files via upload
    tela cadastro com localStorage

---
+++ codigo-fonte/telaCadastro/telacadastro.js
@@ -0,0 +1,93 @@
+function cadastrar() { 
+    if (nome.value == "" || nome.value.length < 4) {
+      alert("Preencha o formulário corretamente!");
+      nome.focus();
+      return;
+    }
+    if (cpf.value == "" || cpf.value.length < 4) {
+        alert("Preencha o formulário corretamente!");
+        cpf.focus();
+        return;
+      }
+      if (rg.value == "" || rg.value.length < 4) {
+        alert("Preencha o formulário corretamente!");
+        rg.focus();
+        return;
+      }
+    if (email.value == "") {
+      alert("Preencha o formulário corretamente!");
+      email.focus();
+      return;
+    }
+    if (telefone.value == "" || telefone.value.length < 11) {
+      alert("Preencha o formulário corretamente!");
+      telefone.focus();
+      return;
+    }
+    if (endereco.value == "" || endereco.value.length < 10) {
+      alert("Preencha o formulário corretamente!");
+      endereco.focus();
+      return;
+    }
+    
+      if (numero.value == "" || numero.value.length < 2) {
+        alert("Preencha o formulário corretamente!");
+        numero.focus();
+        return;
+      }
+      if (complemento.value == "" || complemento.value.length < 4) {
+        alert("Preencha o formulário corretamente!");
+        complemento.focus();
+        return;
+      }
+      
+      if (bairro.value == "" || bairro.value.length < 4) {
+        alert("Preencha o formulário corretamente!");
+        bairro.focus();
+        return;
+      }
+      if (cidade.value == "" || cidade.value.length < 4) {
+        alert("Preencha o formulário corretamente!");
+        cidade.focus();
+        return;
+      }
+      if (estado.value == "" || estado.value.length < 2) {
+        alert("Preencha o formulário corretamente!");
+        estado.focus();
+        return;
+      }
+    if (Senha.value == "" || Senha.value.length < 6) {
+      alert("Preencha o formulário corretamente!");
+      Senha.focus();
+      return;
+    }
+    if (ConfirmeSenha.value == "") {
+      alert("Preencha o formulário corretamente!");
+      ConfirmeSenha.focus();
+      return;
+    }
+  
+    if (Senha.value != ConfirmeSenha.value) {
+      alert("As senhas não correspondem!");
+      ConfirmeSenha.focus();
+      return;
+    }
+  
+    let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]')
+  
+    listaUser.push(
+      {
+        nomeUser: nome.value,
+        emailUser: formulario.email.value,
+        telefoneUser: formulario.telefone.value,
+
+        enderecoUser: formulario.endereco.value,
+        SenhaUser: formulario.Senha.value,
+        ConfirmeSenhaUser: formulario.ConfirmeSenha.value
+      });
+  
+    localStorage.setItem("listaUser", JSON.stringify(listaUser))
+  
+    alert("Usuário cadastrado com sucesso!");
+    window.location.href = "../pagina-login/login.html"
+  }
