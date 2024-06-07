162eedcea0d99e53bbaff2066fdeac6378a3b672
Gustavo Souza <165229320+Rjangoteia2@users.noreply.github.com>
2024-06-07 18:19:27 -0300

    Update script.js

--- codigo-fonte/loginPage/script.js
+++ codigo-fonte/loginPage/script.js
@@ -2,9 +2,37 @@ function logar(){
     var email = document.getElementById('email').value;
     var senha = document.getElementById('senha').value;
 
-    if(email == "usuario123@outlook.com"  && senha == "usuario"){
+     let listaUser = [] 
+
+
+     let userValid = { 
+
+        email: "",
+        senha: ""
+     }
+ 
+     listaUser = JSON.parse(localStorage.getItem("listaUser"))
+     listaUser.forEach((item) => { 
+      if(email === item.emailUser && senha === item.SenhaUser){
+
+
+        userValid ={
+           email:item.emailUser,
+           senha:item.SenhaUser
+
+        }
+
+      }
+    }) 
+    
+    if(email === userValid.email && senha === userValid.senha && email !== "" && senha !== ""){
         alert('Login bem-sucedido');
-        location.href = "usuarioLogado.html";
+         location.href = "../homePage/index.html";
+
+         let mathRandom = Math.random().toString(16).substr(2) 
+         let token = mathRandom + mathRandom
+         localStorage.setItem("token", token);
+
     }else{
         alert('Usuario ou senha incorretos');
     }    
