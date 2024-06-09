6c6b91deed75478a65f97c7a70d0abaa54abc812
Milton Ventura <milton.venturapsi@gmail.com>
2024-06-09 05:40:14 -0300

    Ajust na página prestadores e alguns detalhes

--- codigo-fonte/prestadores/prestadores.js
+++ codigo-fonte/prestadores/prestadores.js
@@ -1,11 +1,60 @@
-let termPesq = JSON.parse(localStorage.getItem("termPesq"));
+let termPesq = JSON.parse(localStorage.getItem('termPesq'));
+if (termPesq) {
+    termPesq = termPesq.trim().toLowerCase();
+}
+
+function imprimeHeader() {
+    let usrLogado = JSON.parse(localStorage.getItem('usrLogado'))
+
+    if (usrLogado) {
+        logo.onclick = function () {
+            location.reload();
+        }
+        let btnContent = `<img src="imgs/userround.png" alt="Foto de usuário redonda">
+        <button id="logoffbtn" class="logoffbtn">Logout</button>`;
+
+        hdrButtons.innerHTML = btnContent;
+        let logoffbtn = document.getElementById('logoffbtn');
+        if (logoffbtn) {
+            logoffbtn.onclick = function () {
+                localStorage.usrLogado = false;
+                localStorage.id = '';
+                localStorage.token = '';
+                window.location.href = ('../homePage/index.html');
+            }
+        }
+    } else {
+        logo.onclick = function () {
+            window.location.href = ('../homePage/index.html');
+        }
+        btnContent = `<button id="cadastrobtn" class="cadastrobtn">Cadastre-se</button>
+        <button id="loginbtn" class="loginbtn">Login</button>`
+
+        hdrButtons.innerHTML = btnContent;
+        let loginbtn = document.getElementById('loginbtn');
+        if (loginbtn) {
+            loginbtn.onclick = function () {
+                window.location.href = ('../loginPage/login.html');
+            }
+        }
+        let cadastrobtn = document.getElementById('cadastrobtn');
+        if (cadastrobtn) {
+            cadastrobtn.onclick = function () {
+                window.location.href = ('../cadastro/cadastro.html');
+            }
+        }
+
+    }
+
+}
 
 searchButton.onclick = function () {
     localStorage.termPesq = JSON.stringify(searchBar.value);
     if (!window.location.href.endsWith('prestadores.html')) {
         window.location.href = ('../prestadores/prestadores.html');
     }
-    termPesq = JSON.parse(localStorage.getItem("termPesq"));
+    termPesq = JSON.parse(localStorage.getItem('termPesq'));
+    termPesq = termPesq.trim().toLowerCase();
     imprimePrestadores(termPesq);
 };
 
@@ -13,24 +62,6 @@ function retornaListaUsers() {
     let listaUsuarios = [];
     listaUsuarios = JSON.parse(localStorage.getItem("arrUsuarios")) || []
     return listaUsuarios;
-}
-
-
-function addUsuario(nomeusr, distancia, prestServ, servsPrestados) {
-    listaUsuarios = retornaListaUsers();
-    let usuario = {
-        nome: nomeusr,
-        distancia: distancia,
-        prestadorServico: prestServ,
-        servicosPrestados: servsPrestados || []
-    };
-    listaUsuarios.push(usuario);
-    localStorage.arrUsuarios = JSON.stringify(listaUsuarios);
-};
-
-function limpaLocalStorage() {
-    listaUsuarios = [];
-    localStorage.arrUsuarios = JSON.stringify(listaUsuarios);
 }
 
 function retornaPrestadores() {
@@ -46,48 +77,64 @@ function retornaPrestadores() {
 }
 
 function imprimePrestadores(termPesq = "") {
+    termPesq = termPesq.trim().toLowerCase();
     let conteudo = "";
     let listaPrestadores = retornaPrestadores();
-    if (termPesq){
+    if (termPesq) {
         let listaPesquisada = [];
-        for(prestador of listaPrestadores){
-            dadosPrestador = Object.values(prestador).map(dado => dado.toString());
-            dadosPrestador.splice(2, 2);
+        for (prestador of listaPrestadores) {
+            dadosPrestador = Object.values(prestador).map(dado => dado.toString().trim().toLowerCase());
             dadosPrestador.map((dado) => {
-                if (dado.includes(termPesq)){
-                    listaPesquisada.push(prestador);
-                    return;
-                }
-            })
-            for (servico of prestador.servicosPrestados){
-                dadosServico = Object.values(servico).map(dado => dado.toString());
-                dadosServico.map((dado) => {
-                    if (dado.includes(termPesq)){
+                if (dado.includes(termPesq)) {
+                    let prestadorPresente = false
+                    for (prestadorResult of listaPesquisada) {
+                        if (prestadorResult == prestador) {
+                            prestadorPresente = true
+                        }
+                    }
+                    if (!prestadorPresente) {
                         listaPesquisada.push(prestador);
-                        return;
+                    }
+                };
+            });
+            for (servico of prestador.servicosPrestados) {
+                dadosServico = Object.values(servico).map(dado => dado.toString().trim().toLowerCase());
+                dadosServico.map((dado) => {
+                    if (dado.includes(termPesq)) {
+                        prestadorPresente = false
+                        for (prestadorResult of listaPesquisada) {
+                            if (prestadorResult == prestador) {
+                                prestadorPresente = true
+                            }
+                        }
+                        if (!prestadorPresente) {
+                            listaPesquisada.push(prestador);
+                        }
                     }
                 })
             }
         }
         listaPrestadores = listaPesquisada;
     }
-            
+
     listaPrestadores.map((prestador) => {
         prestador.servicosPrestados.map((servico) => {
-            let valor = "Valor a combinar";
+            let valor = 'Valor a combinar';
             if (servico.valor) {
-                valor = `R$${servico.valor.toFixed(2).replace(".", ",")} / hora`;
+                valor = `R$${parseFloat(servico.valor).toFixed(2).replace(".", ",")} / hora`;
             }
+            let avaliacao = parseFloat(servico.avaliacao).toFixed(1).replace(".", "s");
+            let distancia = retornaNumRandom();
             conteudo += `
             <div class="prestadores">
                 <img src="imgs/userround.png" alt="Foto usuário redondo">
                 <div class="prestadoresdscr">
                     <p>${prestador.nome}, <strong>${servico.nomeServPrestado}</strong> <img src="imgs/map-pin.png" alt="Map pin">
-                        ${prestador.distancia}Km
+                        ${distancia}Km
                         <br><br>${servico.descricao}
                     </p>
                     <div class="cardpreco">
-                        <div class="stars"><img src="imgs/4stars.png" alt="Estralas de avaliação"></div>
+                        <div class="stars"><img src="imgs/${avaliacao}stars.png" alt="Estralas de avaliação"></div>
                         <p class="price">${valor}</p>
                         <p class="pricenote">(preço médio)</p>
                     </div>
@@ -96,6 +143,32 @@ function imprimePrestadores(termPesq = "") {
         });
     });
     cardPrestadores.innerHTML = conteudo;
+    localStorage.termPesq = JSON.stringify('');
+    searchBar.value = '';
+}
+
+cardPrestadores.onclick = function () {
+    let usrLogado = JSON.parse(localStorage.getItem('usrLogado'));
+    if (usrLogado){
+        window.location.href = ('../chatPage/chat.html');
+    }else{
+        window.location.href = ('../cadastro/cadastro.html')
+    }
+}
+
+function retornaNumRandom() {
+    number = Math.floor(Math.random() * ((20 - 0) / 0.5)) * 0.5;
+    return parseInt(number).toString();
+}
+
+let imgBanner = document.querySelectorAll('#imgbanner');
+let valorBanner = document.querySelectorAll('#txtbanner');
+for(let i = 0; i < imgBanner.length; i++){
+    imgBanner[i].addEventListener('click', function(){
+        imprimePrestadores(valorBanner[i].textContent);
+    })
 }
 
 imprimePrestadores(termPesq);
+imprimeHeader();
+
